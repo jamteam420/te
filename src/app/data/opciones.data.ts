0869fafcb3f6aa36964d7f5505ea643b649b67e6
jamteam420 <ccjamteam@gmail.com>
2026-02-26 16:14:05 -0300

    chore: actualizar datos, environment prod y remover PATs del doc de deploy

--- src/app/data/opciones.data.ts
+++ src/app/data/opciones.data.ts
@@ -4,28 +4,47 @@
  */
 
 export const SOCIOS: string[] = [
-  'Juan Pérez',
-  'María García',
-  'Carlos López',
-  'Ana Martínez',
-  'Pedro Rodríguez',
+  "Andres Godoy (Pirotto)",
+  "Andres Seferian",
+  "Bruno Tort (Sabri)",
+  "Claudia Buslón",
+  "Claudio Flores",
+  "Cristian Gilrosconi (Nórdico)",
+  "Dayana Perez",
+  "Emilio Luengo (Canario)",
+  "Felipe García",
+  "Franco Migues (UTE)",
+  "Hermano Vale",
+  "Isabel Valentín (Tom)",
+  "Javier Magallanes (Tato)",
+  "Joaquin Rodríguez",
+  "José Aznares",
+  "Mario Alvez",
+  "Martín Ross",
+  "Matias Caamaño",
+  "Maximiliano Giorge",
+  "Micaela Rivero (Hna Martín)",
+  "Nicolas Castiñeira (Peludo)",
+  "Pablo da Costa",
+  "Pablo Flores",
+  "Pablo Toche",
+  "Patricia Anselmi",
+  "Santiago Vidal (Vita)",
+  "Sebastian Bilche",
+  "Theo Carlevaro"
 ];
 
 export const VARIEDADES: string[] = [
-  'Gyokuro',
-  'Sencha',
-  'Matcha',
-  'Oolong',
-  'Pu-erh',
-  'Earl Grey',
-  'Darjeeling',
-  'Chai',
-  'Rooibos',
+  'Mindflayer',
+  '24K x Kosher',
+  'Apple Cobbler',
+  'Ice Cream Cake'
 ];
 
 export const PAGADORES: string[] = [
-  'Efectivo en caja',
-  'Juan Pérez',
-  'María García',
-  'Carlos López',
+  'Caja Cash',
+  'Caja Tato',
+  'Caja Martin',
+  'Caja Andi',
+  'Caja Felipe'
 ];
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  googleScriptUrl: 'URL_DEL_APPS_SCRIPT', // Reemplazar con la URL de tu Google Apps Script
+  googleScriptUrl: 'https://script.google.com/macros/s/AKfycbzqNghebwlOogDckI4OxQSVAiEfUDFkrCggX3r2Go2-tHlpwF4o5YzeW3oWNhV8ujYQmg/exec', // Reemplazar con la URL de tu Google Apps Script
 };
