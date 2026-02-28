43808adff4a0cb4324d513860ce1cbc05bf8e2da
jamteam420 <ccjamteam@gmail.com>
2026-02-28 02:33:04 -0300

    deploy: actualización

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  googleScriptUrl: 'https://script.google.com/macros/s/AKfycbxqVeZYjDZfcHRkDrk62uA8Ra23s4bY8L6pkcdu72q7bsMy-cqOT_ww9K8MbgaJNh15QA/exec', // Reemplazar con la URL de tu Google Apps Script
-};
+  googleScriptUrl: 'https://script.google.com/macros/s/AKfycbwVbxSXOe365Zx5ZXf63bMH1JtsUcFcXy0sfZf5vsNf7FwvHrwxp_G4PYnsD3vcymjJ/exec'
+};©
