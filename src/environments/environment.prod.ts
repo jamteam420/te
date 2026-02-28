dcde72957226d6cfb37e9aed02d9c2b9f5577d06
jamteam420 <ccjamteam@gmail.com>
2026-02-28 02:33:43 -0300

    deploy: actualización

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
   googleScriptUrl: 'https://script.google.com/macros/s/AKfycbwVbxSXOe365Zx5ZXf63bMH1JtsUcFcXy0sfZf5vsNf7FwvHrwxp_G4PYnsD3vcymjJ/exec'
-};©
+};
